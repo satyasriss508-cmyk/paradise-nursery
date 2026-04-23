2a45b8f6c8805c090f333a0658c89bb766b1c708
satyasriss508-cmyk <satyasriss508@gmail.com>
2026-04-24 02:23:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
-    <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+    <div className="background-image">
+      {!showProductList ? (
+        <div>
+          <h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProductList(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
